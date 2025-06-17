6a6a56cd617b30bdab8267b124d8a2bb26d66238
Chris <abbottci@mail.uc.edu>
2025-06-17 13:39:42 -0400

    Initial commit

---
+++ src/TheaterBand.java
@@ -0,0 +1,13 @@
+public class TheaterBand
+{
+    public static void main(String[] args)
+    {
+        final int gets_band = 21;
+        int customer_age = 22;
+
+        if (customer_age >= gets_band)
+        {
+            System.out.println("You get a wrist band!");
+        }
+    }
+}
